014a3d235e076321217fec77c84050c63a7392e0
Fabian Castaneda <fabian.castaneda@abits.com>
2017-06-07 16:56:16 -0500

    Version 2

--- main.js
+++ main.js
@@ -5,48 +5,57 @@ var config = {
     }
 }
 
+axios.defaults.baseURL = 'http://localhost:8080/';
 
 new Vue({
     el: '#app',
     data: {
-        users: [],
-        errors: [],
+        users: [],        
         searchResult: [],        
-        searchId: '',
-        lname: '',
-        fname: '',
+        searchByName: '',  
+        lastName: '',
+        firstName: '',
         email: '',
         phone: '',
-        d_lname: '',
-        d_fname: '',
-        d_email: '',
-        d_phone: '',
+        editInputFirstName: '',
+        editInputLastName: '',
+        editInputEmail: '',
+        editInputPhone: '',
+        editIsDeleted: false,
+        editIsEditing: false,
         pagination: { currentPage: 1 },        
+        totalItems: 0,
         setPage: function(pageNo) {
-            this.pagination.currentPage = pageNo
+            console.log('setPage....')            
+            this.pageChanged()
+            this.searchByName = ''
         },
         pageChanged: function() {
-            console.log('Page changed to: ' + this.pagination.currentPage)
-               
-            axios.get(`http://localhost:8080/data`)
+            console.log('pageChanged.......')
+
+            axios.get(`data?is_deleted=false`)
                 .then(response => {
-                    //console.log(response.data)
                                     
                     this.users = response.data
-             
-                    var start = (this.pagination.currentPage -1) * 2 
-                    this.users =  this.users.slice(start, start + 2)
+                    this.totalItems = this.users.length
+                    
+                    var start = (this.pagination.currentPage -1) * 10 
+                    this.users =  this.users.slice(start, start + 10)
 
                 })
                 .catch(e => {
                     console.log('error -> ' + e)
-                    this.errors.push(e)
+                    
                 })
                
             
-        },
-        totalItems: 100
+        }
         
+
+    },
+    mounted() {
+
+        this.loadData()
 
     },    
     computed: {               
@@ -56,136 +65,125 @@ new Vue({
         }
     },
     methods: {
-        /*getList() {
+        loadData (locate_in) {
+            if(locate_in == null) locate_in = 'def'
 
-            axios.get(`http://localhost:8080/data`)
+            console.log('loadData....' + locate_in)
+            this.pageChanged()
+
+        },
+        editUser(user_in) {
+                    
+            var responseObject = []
+            user_in.is_editing = true            
+
+            // Performing a GET request
+            axios.get(`data/` + user_in.id)
                 .then(response => {
-                    //console.log(response.data)
-                                    
-                    this.users = response.data
-                    this.users = this.users.slice(0, 3) 
+                    
+                    responseObject = response.data                                                    
+                    this.editInputFirstName = responseObject["firstname"].toString()
+                    this.editInputLastName = responseObject["lastname"].toString()
+                    this.editInputEmail = responseObject["email"].toString()
+                    this.editInputPhone = responseObject["phone"].toString()
+                    this.editIsEditing = responseObject["is_editing"]
+                    this.editIsDeleted = responseObject["is_deleted"]
 
                 })
                 .catch(e => {
-                    console.log('error -> ' + e)
-                    this.errors.push(e)
+                   console.log('err->' + e)
+
                 })
 
-        },*/
-        editUser(user_in) {
-
-            user_in.is_editing = '1'
-            this.d_fname = user_in.firstname
-            this.d_lname = user_in.lastname
-            this.d_email = user_in.email
-            this.d_phone = user_in.phone
+                        
         },
         discardEdit(user_in) {
-            user_in.is_editing = '0'
+            user_in.is_editing = false
             
 
         },
-        insertUsert(){       
-                       
-            axios.post('http://localhost:8080/data/', { 
-                firstname: this.fname,
-                lastname: this.lname,
+        insertUsert(){    
+            var self = this
+
+            // Performing a POST request         
+            axios.post('data/', { 
+                firstname: this.firstName,
+                lastname: this.lastName,
                 email: this.email,
-                phone: this.phone,
-                is_deleted: '0',
-                is_editing: '0'
+                phone: this.phone.toString(),
+                is_deleted: this.editIsDeleted,
+                is_editing: this.editIsEditing
 
             }).then(function(response){
-                console.log('register successfully')
-
+                alert('Registro exitoso (-.-)/ ')
+                self.loadData()
+                
             })
-
-            this.users.push({
-                firstname: this.fname,
-                lastname: this.lname,
-                email: this.email,
-                phone: this.phone,
-                is_deleted: '0',
-                is_editing: '0'
-            })  
-
-            this.fname = '' 
-            this.lname = ''
+            
+            this.firstName = '' 
+            this.lastName = ''
             this.email = ''
             this.phone = ''                       
             
         },
-        updateUser(user_in) {
-            //console.log('data ' + user_in.id, user_in.firstname, user_in.lastname)
+        updateUser(user_in) {            
+            var locate = 'fn >> updateUser'  
+            var self = this
+
             // Performing a PUT request
-            axios.put('http://localhost:8080/data/' + user_in.id, {                    
-                    firstname: this.d_fname,
-                    lastname: this.d_lname,
-                    email: this.d_email,
-                    phone: this.d_phone,
-                    is_deleted: '0',
-                    is_editing: '0'
+            axios.put('data/' + user_in.id, {   
+
+                    firstname: this.editInputFirstName,
+                    lastname: this.editInputLastName,
+                    email: this.editInputEmail,
+                    phone: this.editInputPhone,
+                    is_deleted: this.editIsDeleted,
+                    is_editing: this.editIsEditing
                   
-                })
-                .then(function(response) {
-                    console.log('saved successfully')
+                }
+            )
+            .then(function(response) {               
+                user_in.is_editing = false
+                alert('Elemento actualizado correctamente (-.-)/ ')            
+                self.loadData()
+            })
 
-                })
-
-            user_in.firstname = this.d_fname
-            user_in.lastname = this.d_lname
-            user_in.email = this.d_email
-            user_in.phone = this.d_phone                  
-            user_in.is_deleted == '0'
-            user_in.is_editing == '0'
-            
-            
 
         },
         deleteUser(user_in) {
+            var self = this
+            var message = confirm("Estas seguro de eliminar el registro?");
+             
+            if (message) {
 
-            var mensaje = confirm("Estas seguro de eliminar el registro?");
-
-            if (mensaje) {
-
-                axios.delete('http://localhost:8080/data/' + user_in.id, )
+                axios.delete('data/' + user_in.id, )
                 .then(function(response) {
-                    console.log('delete successfully!!')
-
+                    alert('Elemento borrado correctamente (-.-)/ !!')
+                    self.loadData()
                 })
-
-                user_in.is_editing = '2'
+                
 
             }else {
                 console.log('Accion cancelada')
             }
         },
-        searchUser(){            
-            console.log(this.searchId)
-            
-
-            axios.get(`http://localhost:8080/data/` + this.searchId)
+        searchUser(){                          
+            // Performing a GET request
+            axios.get(`data?firstname=` + this.searchByName)
                 .then(response => {
 
-                    console.log(response)
-                    this.searchResult = response.data            
+                    console.log('response.data>>>' + response.data )
+                    this.users = response.data
 
                 })
                 .catch(e => {
-                    this.searchResult = []
+                    console.log('err->' + e)
 
-                }) 
-        },
-        loadData () {
-            this.pageChanged()
-
+                })
+                            
         }
 
 
-    },    
-    mounted() {
-        //this.getList()
-        this.loadData()
     }
 })
     
